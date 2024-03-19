e9b9da20828b9e50b02e4ca5bf6dba52d9f60a93
Svante-Bengtsson <152278814+Svante-Bengtsson@users.noreply.github.com>
2024-03-19 14:21:21 +0100

    19/03/2024/14:21
    helt klar med sida 2

--- js/main.js
+++ js/main.js
@@ -1,6 +1,30 @@
-function myFunction() {
+function hamburgermeny() {
   var x = document.getElementById("menu-items");
   var menuIcon = document.querySelector('.hamburger-menu');
   x.classList.toggle('open');
   menuIcon.classList.toggle('change');
 }
+
+document.querySelectorAll('.click1').forEach(e => {
+  let container1 = document.querySelector(".container1")
+  var click2 = document.querySelector('.click2');
+  e.addEventListener('click', () => {
+    e.parentElement.children[1].classList.toggle('hide');
+    click2.parentElement.children[1].classList.add('hide');
+    console.log(click2.parentElement.children[1]);
+    container1.classList.remove('contchange2')
+    container1.classList.toggle('contchange1')
+  })
+});
+document.querySelectorAll('.click2').forEach(e => {
+  var container1 = document.querySelector(".container1")
+  var click1 = document.querySelector('.click1');
+  e.addEventListener('click', () => {
+    e.parentElement.children[1].classList.toggle('hide');
+    console.log(click1.parentElement.children[1]);
+    click1.parentElement.children[1].classList.add('hide');
+    container1.classList.remove('contchange1')
+    container1.classList.toggle('contchange2')
+
+  })
+});
