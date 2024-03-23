f58bcbb09014370e3d3385fad5b1b0367a2b0d13
Svante-Bengtsson <152278814+Svante-Bengtsson@users.noreply.github.com>
2024-03-23 15:40:05 +0100

    2024-03-23-15:39
    carousel klar

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+let slideIndex = 1;
+let slides = document.getElementsByClassName("slide");
 function hamburgermeny() {
   var x = document.getElementById("menu-items");
   var menuIcon = document.querySelector('.hamburger-menu');
@@ -37,4 +39,20 @@ function stickyaside() {
   } else {
     aside.classList.remove("sticky");
   }
+}
+
+
+showSlides(slideIndex);
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+function showSlides(n) {
+  let i;
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  
+  slides[slideIndex-1].style.display = "block";
 }
